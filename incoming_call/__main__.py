a57f6cb92b96e2e9f23c75814ffdef61d1055f8b
Benjamin Boll <github@b-sqr.de>
2022-10-13 13:52:44 +0200

    :recycle: extract variables to .env
    Co-authored-by: Luca van der Will <vanderwill@sipgate.de>
    Co-authored-by: Tobias Vossen <tvossen@sipgate.de>

--- incoming_call/__main__.py
+++ incoming_call/__main__.py
@@ -1,4 +1,9 @@
 import incoming_call.server as server
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+WEBHOOK_PORT = os.environ.get("WEBHOOK_PORT")
 
 if __name__ == "__main__":
-    server.app.run(port=8080)
+    server.app.run(port=WEBHOOK_PORT)
